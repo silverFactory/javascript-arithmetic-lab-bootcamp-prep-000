332255c8b77cd2b37aaec629cb6d9fcbba2e0c06
Will Studabaker <will.studabaker@gmail.com>
2020-10-15 19:23:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(a, b){
+  return a + b
+}
+function subtract(a, b){
+  return a - b
+}
+function multiply(a, b){
+  return a * b
+}
+function divide(a, b){
+  return a / b
+}
+function makeInt(string){
+  return parseInt(string)
+}
+function preserveDecimal(string){
+  return parseFloat(string)
+}
