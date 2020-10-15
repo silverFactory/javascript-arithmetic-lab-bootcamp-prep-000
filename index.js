20acaa09216fc4d5564b62e017cc7d4aef7fda07
Will Studabaker <will.studabaker@gmail.com>
2020-10-15 19:25:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ function multiply(a, b){
 function divide(a, b){
   return a / b
 }
+function increment(n){
+return  n +=
+}
+function decrement(n){
+  return n-=
+}
 function makeInt(string){
-  return parseInt(string)
+  return parseInt(string, 10)
 }
 function preserveDecimal(string){
   return parseFloat(string)
